c61b1db963716aefb4f83697b8f4c616e11d8ef1
Caio Felipe <devcaiofelipe15@gmail.com>
2020-12-02 11:16:15 -0300

    Fixing bug on loginRequired

--- src/app/middleware/loginRequired.js
+++ src/app/middleware/loginRequired.js
@@ -1,5 +1,6 @@
 import jwtConfig from '../../config/jwt';
 import jwt from 'jsonwebtoken';
+import User from '../models/user-model';
 
 
 export default async (req, res, next) => {
@@ -8,11 +9,18 @@ export default async (req, res, next) => {
   if(!tokenHeader) {
     return res.status(400).json({ error: "Token not provided" });
   };
-    
+
   const [, token] = tokenHeader.split(' ');
 
   try {
     const tokenDecoded = jwt.verify(token, jwtConfig.secret);
+
+    const userIsActive = await User.findOne({ where: { id: tokenDecoded.id, active: true } });
+
+    if(!userIsActive) {
+      return res.status(401).json({ info: 'You need active your account' })
+    };
+    
     req.userId = tokenDecoded.id;
     return next();
   } catch(error) {
